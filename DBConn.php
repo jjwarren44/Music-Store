f8f85102b291599140bbe205bf0a3dc5c282b617
11AKFitz1 <43857890+11AKFitz1@users.noreply.github.com>
2019-11-25 01:28:21 -0500

    Possible DB Connection using php
    This is potential after we talk about it.

---
+++ DBConn.php
@@ -0,0 +1,16 @@
+<?php
+
+define('DB_Server','localhost:3306');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define('DB_DATABASE','music_store_db');
+
+$conn = mysqli_connect(DB_Server,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+if (!$conn){
+    die('DB connection failed').mysqli_error($conn);
+}
+$select_db = mysqli_select_db($conn, 'music_store_db');
+if (!$select_db){
+    die('DB selection failed').mysqli_error($conn);
+}
+?>
