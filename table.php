bc2f59ab01c9ab19947b8ea404d615b4d542704a
jjwarren44 <jjwarren44@gmail.com>
2019-11-25 20:03:43 -0500

    Some formatting and search added

--- table.php
+++ table.php
@@ -13,6 +13,32 @@ include('DBConn.php')
 	 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <!-- Script for handling users search -->
+    <script>
+    function search() {
+      // Declare variables
+      var input, filter, table, tr, td, i, txtValue;
+      input = document.getElementById("searchBar");
+      filter = input.value.toUpperCase();
+      table = document.getElementById("albums");
+      tr = table.getElementsByTagName("tr");
+
+      // Loop through all table rows, and hide those who don't match the search query
+      for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        }
+      }
+    }
+    </script>
+
 </head>
 <body>
     <?php
@@ -25,7 +51,7 @@ include('DBConn.php')
             <div class=col-4></div>
             <div class="col-4" align="center">
                 <div class="form-group" align="center">
-                    <input type="text" class="form-control" placeholder="Search Music" id="inputDefault" align="center">
+                    <input type="text" class="form-control" onkeyup="search()" placeholder="Search Music" id="searchBar" align="center">
                 </div>
             </div>
         </div>
@@ -34,38 +60,32 @@ include('DBConn.php')
             </div>
 			<div class="col-10">
                 <!--Will get switched to php code//placeholder -->
-				<table class="table table-borderless" align="Center">
+				<table class="table table-borderless" id="albums" align="Center">
 				<thead>
 						<tr class="table-active">
-						<th scope="col">#</th>
-     					<th scope="col">Song</th>
-      					<th scope="col">Artist</th>
-     					<th scope="col">Album</th>
-                        <th scope="col">Genre</th>
+     					<th scope="col">AlbumID</th>
+      					<th scope="col">Album Title</th>
+     					<th scope="col">ArtistID</th>
      				</tr>
      				</thead>
-     				<tbody>
-     					<tr class="table-active">
-    					<th scope="row">1</th>
-    					<td>Gravity</td>
-    					<td>Against the Current</td>
-    					<td>Gravity</td>
-                        <td>Rock</td>
-    				</tr>
-    				<tr class="table-active">
-    					<th scope="row">2</th>
-    					<td>FireProof</td>
-    					<td>Against the Current</td>
-    					<td>Gravity</td>
-                        <td>rock</td>
-    				</tr>
-    				<tr class="table-active">
-    					<th scope="row">3</th>
-    					<td>Paralyzed</td>
-    					<td>Against the Current</td>
-    					<td>Gravity</td>
-                        <td>Rock</td>
-    				</tr>
+                    <tbody> 
+                    <!-- Begin looping through all records in database to display songs and artist -->
+                    <?php
+
+                        mysqli_begin_transaction($conn); // Start mySQLi transaction
+                        $result = mysqli_query($conn, "SELECT AlbumId, Title, ArtistId FROM Album");
+
+                        $i = 0; // Counter to determine what row we are in
+
+                        while ($row = $result->fetch_assoc()) {
+                            echo "<tr class = 'table-active'>";
+                            echo "<td>".$row['AlbumId']."</td>";
+                            echo "<td>".$row['Title']."</td>";
+                            echo "<td>".$row['ArtistId']."</td>";
+                            $i++;
+                        }
+
+                    ?>
     				</tbody>
     			</table>
     		</div>
